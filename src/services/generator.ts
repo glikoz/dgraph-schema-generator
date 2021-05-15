8dc9333ec6b16337a66db7eab36102ed4aebb85e
glikoz <glikoz@gmail.com>
2021-05-15 09:24:07 +0300

    Co-authored-by: Erdem Gönül <erdemgonul@users.noreply.github.com>

--- src/services/generator.ts
+++ src/services/generator.ts
@@ -1,1 +1,15 @@
-export{}
+import { stringify } from "postcss";
+import { Repository } from "./repository";
+
+export class Generator{
+ constructor(rep:Repository) {
+     
+     const edges=rep.GetEdgeMetadatas();
+     const nodes=rep.GetMetadatas();
+
+     const typeDict=new Map();
+     edges.forEach(edge => {
+         
+     });
+ }   
+}

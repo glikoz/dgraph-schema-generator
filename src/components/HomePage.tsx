498339dc2831d2bc082fcfd0e78443bb4dfeb618
erdemgonul <ahmeterdemgonul@gmail.com>
2021-05-15 18:26:22 +0300

    add properties to node table

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from "react";
 import {
   Metadata,
   Repository,
-  Scalars,
   Property,
 } from "../services/repository";
+import { MdDelete } from "react-icons/md";
+import { toast } from "react-toastify";
 
 const PrimitiveTypes: string[] = ["ID", "String", "Boolean", "Int", "Float"];
 
@@ -12,14 +13,14 @@ type TextProps = {
   text: string;
 };
 type FormProps = {
-  label: string;
-  placeholder: string;
+  label?: string;
+  placeholder?: string;
   value: string;
   onChange: any;
 };
 type SelectProps = {
-  label: string;
-  placeholder: string;
+  label?: string;
+  placeholder?: string;
   options: string[];
   value: string;
   onChange: any;
@@ -27,6 +28,7 @@ type SelectProps = {
 type ButtonProps = {
   onClick: any;
   text: string;
+  color?: string;
 };
 const Title = ({ text }: TextProps) => <h1 className="text-3xl">{text}</h1>;
 const TitleH = ({ text }: TextProps) => <h1 className="text-2xl">{text}</h1>;
@@ -52,9 +54,11 @@ const FormText = ({ label, placeholder, value, onChange }: FormProps) => (
 
 const FormOption = ({ label, options, value, onChange }: SelectProps) => (
   <div className="flex  flex-col items-start my-4">
-    <label className="block text-gray-700 font-bold text-base mb-2">
-      {label}:
-    </label>
+    {label && (
+      <label className="block text-gray-700 font-bold text-base mb-2">
+        {label}:
+      </label>
+    )}
     <select
       className="border rounded h-10 px-4  text-lg font-normal"
       value={value}
@@ -67,9 +71,9 @@ const FormOption = ({ label, options, value, onChange }: SelectProps) => (
   </div>
 );
 
-const FormButton = ({ text, onClick }: ButtonProps) => (
+const FormButton = ({ text, onClick, color }: ButtonProps) => (
   <button
-    className="text-lg px-4 py-1 h-12 text-white bg-pink-400 rounded self-center"
+    className={"text-lg px-4 py-1 h-12 text-white rounded self-center " + color}
     onClick={onClick}
   >
     {text}
@@ -92,15 +96,18 @@ function HomePage() {
       Type: propertyType,
       Name: propertyName,
     };
-    propertyList.push(prop);
+    setPropertyList((arr) => [...arr, prop]);
     console.log(propertyList);
+    toast.success("Property Added!");
   };
 
   const createMetadata = () => {
     const md: Metadata = new Metadata();
-    md.Name = propertyType;
+    md.Name = nodeName;
     md.Properties = propertyList;
     console.log("Metadata", md);
+    window.localStorage.setItem("nodemetadata", JSON.stringify(md));
+    toast.success("Nodemetadata Created!");
   };
 
   return (
@@ -131,20 +138,89 @@ function HomePage() {
             value={propertyType}
             onChange={(e: any) => setPropertyType(e.target.value)}
           />
-          <FormButton text="Add Property" onClick={() => addProperty()} />
+          <FormButton
+            text="Add Property"
+            color="bg-pink-400"
+            onClick={() => addProperty()}
+          />
         </div>
-        <div className="flex flex-col mt-8">
+        <div className="flex flex-col mt-8 items-start ml-8">
           <TitleH text="Properties" />
-          <div className="flex ml-8 mt-4">
-            {propertyList.map((e: any) => (
-              <div className="flex">
-                <p className="text-lg font-semibold ">
-                  {e.propertyName} - {e.propertyType}
-                </p>
+          <div className="flex flex-col ml-2 mt-4">
+            <div className="w-full">
+              <div className="bg-white shadow-md rounded my-6">
+                <table className="w-full table-auto rounded">
+                  <thead>
+                    <tr className="bg-green-500 text-white uppercase text-sm leading-normal">
+                      <th className="py-3 px-6 text-left">Name</th>
+                      <th className="py-3 px-6 text-left">Type</th>
+                      <th className="py-3 px-6 text-center">Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody className="text-gray-600 text-sm font-light">
+                    {propertyList.map((e: any, index: number) => (
+                      <tr className="border-b border-gray-200 bg-gray-50 hover:bg-gray-100">
+                        <td className="py-1 px-6 text-left">
+                          <div className="flex items-center">
+                            <span className="font-medium text-lg -mt-1">
+                              <FormText
+                                value={propertyList[index].Name}
+                                onChange={(a: any) => {
+                                  let prop: Property = new Property();
+                                  prop.Name = a.target.value;
+                                  prop.Type = e.Type;
+                                  propertyList[index] = prop;
+                                  setPropertyList([...propertyList]);
+                                }}
+                              />
+                            </span>
+                          </div>
+                        </td>
+                        <td className="py-1 px-6 text-left">
+                          <div className="flex items-center">
+                            <span>
+                              <FormOption
+                                options={PrimitiveTypes}
+                                value={propertyList[index].Type}
+                                onChange={(a: any) => {
+                                  let prop: Property = new Property();
+                                  prop.Name = e.Name;
+                                  prop.Type = a.target.value;
+                                  propertyList[index] = prop;
+                                  setPropertyList([...propertyList]);
+                                }}
+                              />
+                            </span>
+                          </div>
+                        </td>
+                        <td className="py-1 px-6 text-center items-center justify-center h-full">
+                          <div className="flex items-center justify-items-center justify-center">
+                            <MdDelete
+                              size="25"
+                              className=""
+                              onClick={() => {
+                                propertyList.splice(index, 1);
+                                setPropertyList([...propertyList]);
+                                toast.success("Property Deleted!");
+                              }}
+                            />
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
               </div>
-            ))}
+            </div>
           </div>
         </div>
+      </div>
+      <div className="fixed bottom-4 right-4">
+        <FormButton
+          text="Create NodeMetadata"
+          color="bg-indigo-500"
+          onClick={() => createMetadata()}
+        />
       </div>
     </div>
   );
--- src/services/repository.ts
+++ src/services/repository.ts
@@ -2,7 +2,7 @@ import { specialChars } from "@testing-library/user-event";
 import { Guid } from "guid-typescript"
 export class Repository {
     UpsertMetadata(md:Metadata) {
-        window.localStorage.setItem("metadata:"+md.Id,JSON.stringify(md));
+        //window.localStorage.setItem("metadata:"+md.Id,JSON.stringify(md));
     }
 
     UpsertEdgeMetadata() {

b05bb3ff1d36b5972daa12638db106810b7996af
glikoz <glikoz@gmail.com>
2021-05-15 18:28:04 +0300

    commit

--- src/services/repository.ts
+++ src/services/repository.ts
@@ -1,22 +1,28 @@
 import { specialChars } from "@testing-library/user-event";
 import { Guid } from "guid-typescript"
 export class Repository {
-    UpsertMetadata(md:Metadata) {
-        window.localStorage.setItem("metadata:"+md.Id,JSON.stringify(md));
+    UpsertMetadata(md: Metadata) {
+        window.localStorage.setItem("metadata:" + md.Id, JSON.stringify(md));
     }
 
     UpsertEdgeMetadata() {
 
     }
 
-    GetMetadatas(){
-         
+    GetMetadatas() {
+
     }
-    GetEdgeMetadatas(){
-        
+    GetEdgeMetadatas(): EdgeMetadata[] {
+        return [];
     }
 }
 
+export class EdgeMetadata {
+    From:Metadata[]=[];
+    To:Metadata[]=[];
+    Name:string="";
+    CrossName:string="";
+}
 
 export class Metadata {
     Id: Guid;
